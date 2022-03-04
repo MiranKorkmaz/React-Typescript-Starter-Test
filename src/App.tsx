4ee4c17f4c2b20740e400f78c3705e1614b224f2
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-03-04 15:26:08 +0100

    adding routes to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import {Routes, Route} from "react-router-dom"
 
 import './App.css';
 import { Header } from './components/layout/header/Header';
+import { DishScreen } from './screens/dish-screen/DishScreen';
+import { SportsScreen } from './screens/sports-screen/SportsScreen';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/dishscreen" element={<DishScreen/>} />
+        <Route path="/sportscreen" element={<SportsScreen/>} /> 
+      </Routes>
     </div>
   );
 }
