a8673ec16a19a3283b40e603b57c3200104b5e06
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-03-05 12:24:16 +0100

    adding delete function to dishData

--- src/screens/dish-screen/DishScreen.tsx
+++ src/screens/dish-screen/DishScreen.tsx
@@ -19,11 +19,10 @@ const Title: React.FC<Props> = () => {
 }
 
 
-const Dishes: React.FC<Dish> = ({id, title, description, url}) => {
+const Dishes: React.FC<Dish> = ({id, title, description, url}: {id: string, title: string, description: string, url?: string}) => {
   return (
     <Card>
-        <p>{id}</p>
-        <p>{title}</p>
+        <p>{id}.{title}</p>
         <p>{description}</p>
         {/* <img src={url} alt="Picture of Dish" /> */}
     </Card>
@@ -33,6 +32,7 @@ const Dishes: React.FC<Dish> = ({id, title, description, url}) => {
 
 export const DishScreen: React.FC<Props> = () => {
   const [dish, setDish] = useState([])
+  const [deleteDish, setDeleteDish] = useState([]) 
   useEffect(() => {
     const url = "http://localhost:3000/dishes"
     fetch(url, {
@@ -46,16 +46,36 @@ export const DishScreen: React.FC<Props> = () => {
       setDish(data)
     })
   }, [])
-  return (
-    <div>
-        <Title/>
-        {dish && dish.map((single: Dish) => <Dishes 
-          id={single.id} 
-          key={single.id} 
-          title={single.title} 
-          description={single.description} 
-          url={single.url}
-        />)}
-    </div>
-  )
+  function handleOnDelete(id: string) {
+    const url = `http://localhost:3000/dishes/${id}`
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(data => {
+      setDeleteDish(data)
+    })
+}
+
+
+return (
+  <div>
+      {dish && dish.map((single: Dish) => 
+        <div>
+          <Title />
+            <Dishes 
+            id={single.id} 
+            key={single.id} 
+            title={single.title} 
+            description={single.description} 
+            url={single.url}
+          />
+          <button onClick={(e) => handleOnDelete(single.id)}>Delete</button>
+        </div>
+      )}
+  </div>
+)
 }
